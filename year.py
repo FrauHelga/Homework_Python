6435474621756d79c53ca9666d8fa97f9db8e8ea
pomidorka <pomidorka@MBP-Sasha-4.Dlink>
2019-09-10 21:33:03 +0300

    leap year commit

--- year.py
+++ year.py
@@ -3,25 +3,27 @@
 import sys
 
 def is_year_leap(year):
-	if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
-		return 'Высокосный год'
+	if (year>=0):
+		if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+			return 'Высокосный год'
+		else:
+			return 'Не высокосный год'
 	else:
-		return 'Не высокосный год'
+		return 'Введите положительное число'
 
 
 if __name__ == "__main__":
 
-	for param in sys.argv:
-		print (param)
 	
-	if len(sys.argv) != 2:
-		print("Не введен год")
-		exit(1)
 	
-	try:
-		a =int(sys.argv[1])
-		print(is_year_leap(a))
-	except ValueError:
-		print('Некорректный ввод данных')
+	if len(sys.argv) == 2:
+		try:
+			a =int(sys.argv[1])
+			print(is_year_leap(a))
+		except ValueError:
+			print('Некорректный ввод данных')
+			exit(1)
+	else:
+		print('Введите только год')
 		exit(1)
 	
